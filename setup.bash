e88cec067e91af7b983fc3f128edd7baa64a9ec5
yuhang09 <yuhang09@baidu.com>
2024-02-16 19:54:07 +0800

    add requirements & setup.bash

---
+++ setup.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+###
+ # @Author       : yuhang09 yuhang09@baidu.com
+ # @Date         : 2024-02-16 17:53:53
+ # @LastEditors  : yuhang09 yuhang09@baidu.com
+ # @LastEditTime : 2024-02-16 17:53:54
+ # @FilePath     : /mm_perception/setup.bash
+ # @Description  : 
+ # Copyright (c) Baidu, Inc. and its affiliates. All Rights Reserved
+### 
+
