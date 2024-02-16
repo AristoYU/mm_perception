10481743da0520fde61e219674d4f77df1fcb022
yuhang09 <yuhang09@baidu.com>
2024-02-17 02:28:55 +0800

    add mmdeploy &add mm_perceptron lib

---
+++ mm_perceptron/__init__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+"""
+# @Author: AristoYU
+# @Date: 2024-02-17 02:24:02
+# @LastEditTime: 2024-02-17 02:24:05
+# @LastEditors: AristoYU
+# @Description: 
+# @FilePath: /mm_perception/mm_perceptron/__init__.py
+"""
+
--- setup.bash
+++ setup.bash
@@ -3,7 +3,7 @@
  # @Author       : yuhang09 yuhang09@baidu.com
  # @Date         : 2024-02-16 17:53:53
  # @LastEditors: AristoYU
- # @LastEditTime: 2024-02-17 02:00:26
+ # @LastEditTime: 2024-02-17 02:27:57
  # @FilePath: /mm_perception/setup.bash
  # @Description  : 
  # Copyright (c) Baidu, Inc. and its affiliates. All Rights Reserved
@@ -23,8 +23,7 @@ echo "###################################"
 echo "# STEP 1.1: install mmcv          #"
 echo "###################################"
 cd ./modules/mmcv
-pip3 install -r requirements/optional.txt
-pip3 install -r requirements/runtime.txt
+pip3 install -r requirements/optional.txt -r requirements/runtime.txt
 pip3 install -v -e .
 cd -
 # install mmengine
@@ -48,16 +47,22 @@ echo "###################################"
 echo "# STEP 1.4: install mmdetection   #"
 echo "###################################"
 cd ./modules/mmdetection
-pip3 install -r requirements/optional.txt
-pip3 install -r requirements/runtime.txt
+pip3 install -r requirements/optional.txt -r requirements/runtime.txt
 pip3 install -v -e .
 cd -
 # install mmdetection3d
 echo "###################################"
-echo "# STEP 1.4: install mmdetection3d #"
+echo "# STEP 1.5: install mmdetection3d #"
 echo "###################################"
 cd ./modules/mmdetection3d
-pip3 install -r requirements/optional.txt
-pip3 install -r requirements/runtime.txt
+pip3 install -r requirements/optional.txt -r requirements/runtime.txt
+pip3 install -v -e .
+cd -
+# install mmdeploy
+echo "###################################"
+echo "# STEP 1.6: install mmdeploy      #"
+echo "###################################"
+cd ./modules/mmdeploy
+pip3 install -r requirements/optional.txt -r requirements/runtime.txt
 pip3 install -v -e .
 cd -
